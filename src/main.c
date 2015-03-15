7ba22c7b59b8aa56a271bcc49eebbb0454a82688
AwesomeBen1 <AwesomeBen1@users.noreply.github.com>
2015-03-14 21:19:57 -0700

    Finished battery indicator

--- src/main.c
+++ src/main.c
@@ -1,28 +1,31 @@
-// © Ben C.-K. 2015 or something
+// Made by Ben Chapman-Kish up to and on 2015-03-14
 #include "pebble.h"
-// Oh god what is happening in this program
-// I threw a bunch of stuff together and it's so messy
-// I don't even know how to program in C...
+// Don't judge me for this program, especially if it's the future
+// I'll probably be a much better C programmer in the future, I promise
 
 static Window *s_main_window;
 static TextLayer *s_day_layer, *s_date_layer, *s_time_layer;
-static Layer *s_line_layer/*, *s_bat_rect_layer*/;
+static Layer *s_line_layer, *s_bat_rect_layer;
 static BitmapLayer *s_bt_layer, *s_bat_layer, *s_bat_charging_layer;
 static GBitmap *s_bt_on_bitmap, *s_bt_off_bitmap, *s_bat_bord_bitmap, *s_bat_charging_bitmap, *s_bat_charged_bitmap;
-
-// Why isn't this working?
-// static uint8_t chargenum = 100;
+static BatteryChargeState charge;
 
 static void line_layer_update_callback(Layer *layer, GContext* ctx) {
   graphics_context_set_fill_color(ctx, GColorWhite);
   graphics_fill_rect(ctx, layer_get_bounds(layer), 0, GCornerNone);
 }
 
-/*static void bat_rect_fill(Layer *layer, GContext* ctx) {
-  graphics_context_set_fill_color(ctx, GColorWhite);
-  graphics_fill_rect(ctx, GRect(144-16, 6, chargenum/10, 5), 0, GCornerNone);
-}*/
-// Why isn't it recognizing chargenum?
+static void bat_rect_fill(Layer *layer, GContext* ctx) {
+  // Took way too long to get this working...
+	if (!charge.is_plugged || charge.is_charging) {
+		graphics_context_set_compositing_mode(ctx, GCompOpAssign);
+		graphics_context_set_stroke_color(ctx, GColorBlack);
+		graphics_context_set_fill_color(ctx, GColorWhite);
+		// This GRect's origin is based on the origin of its parent layer
+		// Why didn't I realize this sooner? Duh, that must be what the context is for
+  	graphics_fill_rect(ctx, GRect(0,0, (uint8_t)(charge.charge_percent/10), 4), 0, GCornerNone);
+	}
+}
 
 static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
   // Need to be static because they're used by the system later.
@@ -36,6 +39,7 @@ static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
 	
   strftime(s_date_text, sizeof(s_date_text), "%B %e", tick_time);
   text_layer_set_text(s_date_layer, s_date_text);
+	
 
   char *time_format;
   if (clock_is_24h_style()) {
@@ -60,12 +64,14 @@ static void bt_handler(bool connected) {
     bitmap_layer_set_bitmap(s_bt_layer, s_bt_off_bitmap);
   }
 	// For some reason these don't work with vibes after light...
+	// Must be hidden in the docs somewhere explaining why
 	vibes_long_pulse();
 	light_enable_interaction();
 }
 
 static void battery_handler(BatteryChargeState new_state) {
 	// Display battery charge icon
+	// It's ugly but I can't think of a better way right now
 	if (new_state.is_charging) {
 		layer_set_hidden(bitmap_layer_get_layer(s_bat_charging_layer), false);
 		bitmap_layer_set_bitmap(s_bat_layer, s_bat_bord_bitmap);
@@ -76,11 +82,14 @@ static void battery_handler(BatteryChargeState new_state) {
 		layer_set_hidden(bitmap_layer_get_layer(s_bat_charging_layer), true);
 		bitmap_layer_set_bitmap(s_bat_layer, s_bat_bord_bitmap);
 	}
-	// Can't figure out why this isn't working...
-	/*if (!new_state.is_plugged) {
-		chargenum = new_state.charge_percent;
-		layer_mark_dirty(s_bat_rect_layer);
-	}*/
+	
+	/* If I wanted to display the charge percent (for future reference):
+	static char s_battery_buffer[16];
+	snprintf(s_battery_buffer, sizeof(s_battery_buffer), "%d%% charged", new_state.charge_percent);
+	text_layer_set_text(battery_layer, s_battery_buffer); */
+	
+	charge=new_state;
+	layer_mark_dirty(s_bat_rect_layer);
 }
 
 static void main_window_load(Window *window) {
@@ -115,12 +124,14 @@ static void main_window_load(Window *window) {
 	// Bluetooth indicator
 	s_bt_on_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BT_ON);
 	s_bt_off_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BT_OFF);
-	s_bt_layer = bitmap_layer_create(GRect(5, 3, 12, 11));
+	
+	s_bt_layer = bitmap_layer_create(GRect(3, 3, 12, 11));
 	if (bluetooth_connection_service_peek()) {
   bitmap_layer_set_bitmap(s_bt_layer, s_bt_on_bitmap);
   } else {
   bitmap_layer_set_bitmap(s_bt_layer, s_bt_off_bitmap);
   }
+	
   bitmap_layer_set_alignment(s_bt_layer, GAlignCenter);
   layer_add_child(window_layer, bitmap_layer_get_layer(s_bt_layer));
 	
@@ -140,10 +151,9 @@ static void main_window_load(Window *window) {
   layer_add_child(window_layer, bitmap_layer_get_layer(s_bat_charging_layer));
 	layer_set_hidden(bitmap_layer_get_layer(s_bat_charging_layer), true);
 	
-	// Maybe it works better here?
-	/*static uint8_t chargenum = 100;
-	s_bat_rect_layer = layer_create(GRect(144-16, 6, 10, 5));
-  layer_set_update_proc(s_bat_rect_layer, bat_rect_fill);*/
+	s_bat_rect_layer = layer_create(GRect(144-16, 6, 10, 4));
+  layer_set_update_proc(s_bat_rect_layer, &bat_rect_fill);
+	layer_add_child(window_layer, s_bat_rect_layer);
 }
 
 static void main_window_unload(Window *window) {
@@ -156,7 +166,7 @@ static void main_window_unload(Window *window) {
   gbitmap_destroy(s_bt_on_bitmap);
 	gbitmap_destroy(s_bt_off_bitmap);
 	
-	//layer_destroy(s_bat_rect_layer);
+	layer_destroy(s_bat_rect_layer);
 	bitmap_layer_destroy(s_bat_layer);
 	bitmap_layer_destroy(s_bat_charging_layer);
 	
@@ -174,9 +184,9 @@ static void init() {
   });
   window_stack_push(s_main_window, true);
 
-	bluetooth_connection_service_subscribe(bt_handler);
-	battery_state_service_subscribe(battery_handler);
-  tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
+	bluetooth_connection_service_subscribe(&bt_handler);
+	battery_state_service_subscribe(&battery_handler);
+  tick_timer_service_subscribe(MINUTE_UNIT, &handle_minute_tick);
   
   // Prevent starting blank
 	battery_handler(battery_state_service_peek());
